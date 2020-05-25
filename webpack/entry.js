f819e45f009e4361c266cc4d03552ca19defe46e
Tristan Davey <tristan@tristandavey.com>
2020-05-25 23:12:01 +1000

    Add folding at home info page

--- webpack/entry.js
+++ webpack/entry.js
@@ -9,6 +9,7 @@ import './components/main.css';
 import fontawesome from '@fortawesome/fontawesome';
 import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
 import faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner';
+import faUsers from '@fortawesome/fontawesome-free-solid/faUsers';
 import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook';
 import faSlack from '@fortawesome/fontawesome-free-brands/faSlack';
 import faMeetup from '@fortawesome/fontawesome-free-brands/faMeetup';
@@ -18,6 +19,7 @@ import faCalendar from '@fortawesome/fontawesome-pro-light/faCalendar';
 import faMapMarker from '@fortawesome/fontawesome-pro-light/faMapMarker';
 import faBullhorn from '@fortawesome/fontawesome-pro-light/faBullhorn';
 
-fontawesome.library.add(faEnvelope, faSpinner, faFacebook, faMeetup, faYoutube, faTwitter, faSlack, faCalendar, faMapMarker, faBullhorn);
+
+fontawesome.library.add(faEnvelope, faSpinner, faFacebook, faMeetup, faYoutube, faTwitter, faSlack, faCalendar, faMapMarker, faBullhorn, faUsers);
 
 import * as meetup from './components/meetup';
